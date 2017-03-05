58ac7a0a4060d37982d28161b0b02d8875113647
Tasnim Ali <rajonali44492@gmail.com>
2017-03-05 07:23:28 +0000

    Add files via upload

---
+++ AI/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 ~/home/rajonpi/Workstation/AI/greetings/greetings.py
+python3 ~/home/rajonpi/Workstation/AI/weather/weather.py
+python3 ~/home/rajonpi/Workstation/AI/stocks/stocks.py
+python3 ~/home/rajonpi/Workstation/AI/news/NewsV2.py
+
